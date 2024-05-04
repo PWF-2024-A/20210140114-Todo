8fc85e378c18a9ffd97a6667c9b2e46af4f3406e
RoqiSB <roqisb9@gmail.com>
2024-05-05 02:16:04 +0700

    update, delete data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+use function Laravel\Prompts\select;
+
 class UserController extends Controller
 {
     /**
@@ -16,8 +18,7 @@ class UserController extends Controller
 
         if ($search) {
             $users = User::where(function ($query) use ($search) {
-                $query->where('name', 'like', '%' . $search . '%')
-                    ->orwhere('email', 'like', '%' . $search . '%');
+                $query->where('name', 'like', '%' . $search . '%')->orWhere('email', 'like', '%' . $search . '%');
             })
                 ->orderBy('name')
                 ->where('id', '!=', '1')
@@ -29,7 +30,28 @@ class UserController extends Controller
                 ->paginate(10);
         }
 
-        return view("user.index", compact('users'));
+        return view('user.index', compact('users'));
+    }
+    public function makeadmin(User $user)
+    {
+        $user->timestamps = false;
+        $user->is_admin = true;
+        $user->save();
+
+        return back()->with('success', 'Make admin successfully!');
+    }
+
+    public function removeadmin(User $user)
+    {
+        if ($user->id != 1) {
+            $user->timestamps = false;
+            $user->is_admin = false;
+            $user->save();
+
+            return back()->with('success', 'Remove admin successfully!');
+        } else {
+            return redirect()->route('user.index');
+        }
     }
 
     /**
@@ -77,6 +99,12 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        if ($user->id != 1) {
+            $user->delete();
+
+            return back()->with('success', 'Delete user successfully!');
+        } else {
+            return redirect()->route('user.index')->with('danger', 'Delete user failed!');
+        }
     }
 }
---
+++ tugas6.md
@@ -0,0 +1,34 @@
+#Tugas 5
+
+## 1. Todo Complete
+
+![Alt text](screenshot/tugas6/todocomplete.png)
+
+## 2. Todo Uncomplete
+
+![Alt text](screenshot/tugas6/todouncompleted.png)
+
+## 3. Todo Edited
+
+![Alt text](screenshot/tugas6/edit1.png)
+![Alt text](screenshot/tugas6/edit2.png)
+
+## 4. Todo Deleted
+
+![Alt text](screenshot/tugas6/deleted.png)
+
+## 5. Todo Deleted All Completed Todo
+
+![Alt text](screenshot/tugas6/deleteallcomplete.png)
+
+## 6. User Make Admin
+
+![Alt text](screenshot/tugas6/makeadmin.png)
+
+## 7. User Remove Admin
+
+![Alt text](screenshot/tugas6/removeadmin.png)
+
+## 8. User Deleted
+
+![Alt text](screenshot/tugas6/userdelete.png)
